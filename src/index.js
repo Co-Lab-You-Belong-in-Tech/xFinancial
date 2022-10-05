afcb9a003acc212bcae8ba9efe711a62019942f7
Amine Smahi <md.amine.smahi@gmail.com>
2022-10-05 15:08:37 +0100

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
-import { store } from './redux/store'
-import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
