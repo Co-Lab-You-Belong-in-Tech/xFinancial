9a7dade5f9957049c8fbeb7d008309db32ea358f
Amine Smahi <md.amine.smahi@gmail.com>
2022-10-18 12:48:05 +0100

    Add logger to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
-import goalReducer from './goals/goalsSlice';
+import { logger } from 'redux-logger';
+import goalReducer from './features/goals/goalsSlice';
 import questionsReducer from './features/questions/questionsSlice';
+
+// const logger = ReduxLoggerOptions.createLogger();
 
 const store = configureStore({
   reducer: {
     goal: goalReducer,
     questions: questionsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
